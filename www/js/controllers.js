6b76ab25d54ae94af0f3b35616d0be30c5bf666d
Hassen <chaieb.hassen@yahoo.com>
2015-05-17 13:46:13 -0700

    map fake data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,92 +3,144 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
+    $scope.chats = Chats.all();
+    $scope.remove = function(chat) {
+        Chats.remove(chat);
+    }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
 
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 })
 
 
 
-.controller('MapCtrl', function($scope, $ionicLoading, $compile) {
-	$scope.init =function () {
-        var myLatlng = new google.maps.LatLng(45.460760, -75.680405);
-        var mapOptions = {
-          center: myLatlng,
-          zoom: 10,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById('map'),
-            mapOptions);
+.controller('MapCtrl', function($scope, $ionicLoading, $compile, $element) {
 
-// document.getElementById('map').innerHTML  = JSON.stringify(google);
+    $scope.mapData = [{
+        lat: 45.460760,
+        lng: -75.680405,
+        score: 4,
+        image: '/img/ionic.png',
+    }, {
+        lat: 45.460750,
+        lng: -75.880435,
+        score: 2,
+        image: '/img/ionic.png',
+    }];
 
-      //   var marker = new google.maps.Marker({
-      //       position: myLatlng,
-      //       map: map,
-      //       title: 'Hello World!'
-      //   });
+    $scope.colorMapping = {
+        1: '#E54B00',
+        2: '#FFAF02',
+        3: '#F7E301',
+        4: '#8DEF45',
+        5: '#53D44D'
+    }
+    $scope.init = function() {
+        $scope.$watch('mapData', function(mapData) {
+                if (mapData && mapData.length > 0) {
 
-      //  var populationOptions = {
-      //     strokeColor: '#FF0000',
-      //     strokeOpacity: 0.8,
-      //     strokeWeight: 2,
-      //     fillColor: '#FF0000',
-      //     fillOpacity: 0.35,
-      //     map: map,
-      //     center: myLatlng,
-      //     radius: Math.sqrt(500) * 100
-      //   };
-      //   // Add the circle for this city to the map.
-      //   cityCircle = new google.maps.Circle(populationOptions);
+                    var myLatlng = new google.maps.LatLng(45.460760, -75.680405);
+                    var mapOptions = {
+                        center: myLatlng,
+                        zoom: 10,
+                        mapTypeId: google.maps.MapTypeId.ROADMAP
+                    };
+
+                    var map = new google.maps.Map(document.getElementById('map'),
+                        mapOptions);
+
+                    var infowindow = new google.maps.InfoWindow();
+                    // document.getElementById('map').innerHTML  = JSON.stringify(google);
+                    for (var i = 0; i < mapData.length; i++) {
+                        var center = new google.maps.LatLng(mapData[i].lat, mapData[i].lng);
+                        var marker = new google.maps.Marker({
+                            position: center,
+                            map: map,
+                        });
+
+                        var populationOptions = {
+                            strokeColor: $scope.colorMapping[mapData[i].score],
+                            strokeOpacity: 0.8,
+                            strokeWeight: 0,
+                            fillColor: $scope.colorMapping[mapData[i].score],
+                            fillOpacity: 0.35,
+                            map: map,
+                            center: center,
+                            radius: Math.sqrt(500) * 100
+                        };
+                        // Add the circle for this city to the map.
+                        cityCircle = new google.maps.Circle(populationOptions);
+
+                        var contentString = '<div id="content">' +
+                            '<div id="siteNotice">' +
+                            '</div>' +
+                            '<p><b>Score </b>' + mapData[i].score + '</p>' +
+                            '<div star-rating rating-value="' + (mapData[i].score) + '" max="5" ></div>' +
+                            // '<h1 id="firstHeading" class="firstHeading">Place</h1>'+
+                            '<div id="bodyContent">' +
+                            '<img style="width:100px;height:100px" src="' + mapData[i].image + '"/>' +
+                            '</div>' +
+                            '</div>';
+
+                        marker.popup = $compile(contentString)($scope);
+
+                        google.maps.event.addListener(marker, 'click',
+                            function(marker) {
+                                return function() {
+
+
+                                    infowindow.setContent(marker.popup.html());
+                                    infowindow.open(map, marker);
+                                }
+                            }(marker));
+                    };
 
 
 
+                    // google.maps.event.addListener(map, 'zoom_changed', function() {
+                    //   var zoomLevel = map.getZoom();
+                    //   map.setCenter(myLatlng);
+                    //   infowindow.setContent('Zoom: ' + zoomLevel);
+                    // });
+                    $scope.map = map;
+                } //end if
+            }) //end watch
 
 
-      //   var contentString = '<div id="content">'+
-      // '<div id="siteNotice">'+
-      // '</div>'+
-      // '<h1 id="firstHeading" class="firstHeading">Place</h1>'+
-      // '<div id="bodyContent">'+
-      // '<p><b>Place</b>, This is a good place.</p>'+
-      // '</div>'+
-      // '</div>';
-
-      // var infowindow = new google.maps.InfoWindow({
-      //     content: contentString
-      // });
+    }
 
 
-      // google.maps.event.addListener(marker, 'click', function() {
-      //   infowindow.open(map,marker);
-      // });
+})
 
 
-      // google.maps.event.addListener(map, 'zoom_changed', function() {
-      //   var zoomLevel = map.getZoom();
-      //   map.setCenter(myLatlng);
-      //   infowindow.setContent('Zoom: ' + zoomLevel);
-      // });
-      $scope.map=map;
-
-      }
-      // google.maps.event.addDomListener(window, 'load', initialize);
-      // ionic.Platform.ready(initialize);
-
+.directive('starRating', function() {
+    return {
+        restrict: 'A',
+        template: '<ul class="rating">' +
+            '<li ng-repeat="star in stars" ng-class="star">' +
+            '\u2605' +
+            '</li>' +
+            '</ul>',
+        scope: {
+            ratingValue: '=',
+            max: '='
+        },
+        link: function(scope, elem, attrs) {
+            scope.stars = [];
+            for (var i = 0; i < scope.max; i++) {
+                scope.stars.push({
+                    filled: i < scope.ratingValue
+                });
+            }
+        }
+    }
 });
-
-
